326aa9ae72731779303da5f3a76c91471e165c89
DylanTomberlin <tomberln@uw.edu>
2020-01-08 23:24:34 -0800

    started port file

---
+++ port.h
@@ -0,0 +1,17 @@
+/* File:         port.h
+ * Authors:      Dylan Tomberlin
+ * Organization: Husky Robotics Team
+ * 
+ * This file includes function prototypes for all functions which must be
+ * implemented for each ported device.  Just make a .c file called
+ * port_[device name].c and include this file in it.
+ */
+
+#ifndef _PORT_H
+#define _PORT_H
+
+#include "CANPacket.h"
+
+
+
+#endif //_PORT_H
