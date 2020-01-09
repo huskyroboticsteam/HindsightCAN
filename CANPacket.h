23f02df11d6b9cfb889a688a2aa6aee8c1877df7
Baldstrom <jadenbottemiller@gmail.com>
2020-01-09 11:05:48 -0800

    Changed packet IDs to a working value

--- CANPacket.h
+++ CANPacket.h
@@ -44,52 +44,47 @@ void AssembleHeartbeatPacket(CANPacket *packetToAssemble,
 #define DEVICE_GROUP_GPIO_BOARDS        (uint8_t) 0x06
 
 // Common Mode Packet IDs
-#define ID_COMMON_MODE_HIGH_NIBBLE      (uint8_t) DEVICE_GROUP_BROADCAST << 4
-#define ID_ESTOP                        (uint8_t) ID_COMMON_MODE_HIGH_NIBBLE | 0x00
-#define ID_HEARTBEAT                    (uint8_t) ID_COMMON_MODE_HIGH_NIBBLE | 0x01
-#define ID_FAIL_REPORT                  (uint8_t) ID_COMMON_MODE_HIGH_NIBBLE | 0x02
-#define ID_OVRD_PROTECTION              (uint8_t) ID_COMMON_MODE_HIGH_NIBBLE | 0x03
-#define ID_TELEMETRY_TIMING             (uint8_t) ID_COMMON_MODE_HIGH_NIBBLE | 0x04
-#define ID_TELEMETRY_PULL               (uint8_t) ID_COMMON_MODE_HIGH_NIBBLE | 0x05
-#define ID_TELEMETRY_REPORT             (uint8_t) ID_COMMON_MODE_HIGH_NIBBLE | 0x06
-#define ID_LED_COLOR                    (uint8_t) ID_COMMON_MODE_HIGH_NIBBLE | 0x07
+#define ID_ESTOP                        (uint8_t) 0x00
+#define ID_HEARTBEAT                    (uint8_t) 0x01
+#define ID_FAIL_REPORT                  (uint8_t) 0x02
+#define ID_OVRD_PROTECTION              (uint8_t) 0x03
+#define ID_TELEMETRY_TIMING             (uint8_t) 0x04
+#define ID_TELEMETRY_PULL               (uint8_t) 0x05
+#define ID_TELEMETRY_REPORT             (uint8_t) 0x06
+#define ID_LED_COLOR                    (uint8_t) 0x07
 
 // Motor Unit Packet IDs
-#define ID_MOTOR_UNIT_HIGH_NIBBLE       (uint8_t) DEVICE_GROUP_MOTOR_CONTROL << 4
-#define ID_MOTOR_UNIT_MODE_SEL          (uint8_t) ID_MOTOR_UNIT_HIGH_NIBBLE | 0x00
-#define ID_MOTOR_UNIT_CHIP_TYPE_PULL    (uint8_t) ID_MOTOR_UNIT_HIGH_NIBBLE | 0x01
-#define ID_MOTOR_UNIT_PWM_DIR_SET       (uint8_t) ID_MOTOR_UNIT_HIGH_NIBBLE | 0x02
-#define ID_MOTOR_UNIT_PID_POS_TGT_SET   (uint8_t) ID_MOTOR_UNIT_HIGH_NIBBLE | 0x03
-#define ID_MOTOR_UNIT_PID_P_SET         (uint8_t) ID_MOTOR_UNIT_HIGH_NIBBLE | 0x04
-#define ID_MOTOR_UNIT_PID_I_SET         (uint8_t) ID_MOTOR_UNIT_HIGH_NIBBLE | 0x05
-#define ID_MOTOR_UNIT_PID_D_SET         (uint8_t) ID_MOTOR_UNIT_HIGH_NIBBLE | 0x06
-#define ID_MOTOR_UNIT_INIT              (uint8_t) ID_MOTOR_UNIT_HIGH_NIBBLE | 0x07
-#define ID_MOTOR_UNIT_LIM_ALRT          (uint8_t) ID_MOTOR_UNIT_HIGH_NIBBLE | 0x08
-#define ID_MOTOR_UNIT_ENC_PPJR_SET      (uint8_t) ID_MOTOR_UNIT_HIGH_NIBBLE | 0x09
-#define ID_MOTOR_UNIT_MAX_JNT_REV_SET   (uint8_t) ID_MOTOR_UNIT_HIGH_NIBBLE | 0x0A
+#define ID_MOTOR_UNIT_MODE_SEL          (uint8_t) 0x00
+#define ID_MOTOR_UNIT_CHIP_TYPE_PULL    (uint8_t) 0x01
+#define ID_MOTOR_UNIT_PWM_DIR_SET       (uint8_t) 0x02
+#define ID_MOTOR_UNIT_PID_POS_TGT_SET   (uint8_t) 0x03
+#define ID_MOTOR_UNIT_PID_P_SET         (uint8_t) 0x04
+#define ID_MOTOR_UNIT_PID_I_SET         (uint8_t) 0x05
+#define ID_MOTOR_UNIT_PID_D_SET         (uint8_t) 0x06
+#define ID_MOTOR_UNIT_INIT              (uint8_t) 0x07
+#define ID_MOTOR_UNIT_LIM_ALRT          (uint8_t) 0x08
+#define ID_MOTOR_UNIT_ENC_PPJR_SET      (uint8_t) 0x09
+#define ID_MOTOR_UNIT_MAX_JNT_REV_SET   (uint8_t) 0x0A
 
 // GPIO Board Packet IDs
-#define ID_GPIO_BOARD_HIGH_NIBBLE       (uint8_t) DEVICE_GROUP_GPIO_BOARDS << 4
-#define ID_GPIO_BOARD_PWM_SET_STATE     (uint8_t) ID_GPIO_BOARD_HIGH_NIBBLE | 0x00 
-#define ID_GPIO_BOARD_PWM_SET           (uint8_t) ID_GPIO_BOARD_HIGH_NIBBLE | 0x01
-#define ID_GPIO_BOARD_ADC_EN_SET        (uint8_t) ID_GPIO_BOARD_HIGH_NIBBLE | 0x02
-#define ID_GPIO_BOARD_ADC_READ          (uint8_t) ID_GPIO_BOARD_HIGH_NIBBLE | 0x03
-#define ID_GPIO_BOARD_ADC_READ_RESPONSE (uint8_t) ID_GPIO_BOARD_HIGH_NIBBLE | 0x04
-#define ID_GPIO_BOARD_IO_SET_STATE      (uint8_t) ID_GPIO_BOARD_HIGH_NIBBLE | 0x05
-#define ID_GPIO_BOARD_IO_READ           (uint8_t) ID_GPIO_BOARD_HIGH_NIBBLE | 0x06
-#define ID_GPIO_BOARD_IO_READ_RESPONSE  (uint8_t) ID_GPIO_BOARD_HIGH_NIBBLE | 0x07
-#define ID_GPIO_BOARD_IO_WRITE          (uint8_t) ID_GPIO_BOARD_HIGH_NIBBLE | 0x08
+#define ID_GPIO_BOARD_PWM_SET_STATE     (uint8_t) 0x00 
+#define ID_GPIO_BOARD_PWM_SET           (uint8_t) 0x01
+#define ID_GPIO_BOARD_ADC_EN_SET        (uint8_t) 0x02
+#define ID_GPIO_BOARD_ADC_READ          (uint8_t) 0x03
+#define ID_GPIO_BOARD_ADC_READ_RESPONSE (uint8_t) 0x04
+#define ID_GPIO_BOARD_IO_SET_STATE      (uint8_t) 0x05
+#define ID_GPIO_BOARD_IO_READ           (uint8_t) 0x06
+#define ID_GPIO_BOARD_IO_READ_RESPONSE  (uint8_t) 0x07
+#define ID_GPIO_BOARD_IO_WRITE          (uint8_t) 0x08
 
 // Power Distribution Packet IDs
-#define ID_POWER_DIST_HIGH_NIBBLE       (uint8_t) DEVICE_GROUP_POWER << 4
-#define ID_POWER_DIST_RAIL_SET_STATE    (uint8_t) ID_POWER_DIST_HIGH_NIBBLE | 0x00
-#define ID_POWER_DIST_RAIL_REQ_STATE    (uint8_t) ID_POWER_DIST_HIGH_NIBBLE | 0x01
-#define ID_POWER_DIST_RAIL_RESPONSE     (uint8_t) ID_POWER_DIST_HIGH_NIBBLE | 0x02
-#define ID_POWER_DIST_OVC_LIM_SET       (uint8_t) ID_POWER_DIST_HIGH_NIBBLE | 0x03
+#define ID_POWER_DIST_RAIL_SET_STATE    (uint8_t) 0x00
+#define ID_POWER_DIST_RAIL_REQ_STATE    (uint8_t) 0x01
+#define ID_POWER_DIST_RAIL_RESPONSE     (uint8_t) 0x02
+#define ID_POWER_DIST_OVC_LIM_SET       (uint8_t) 0x03
 
 // Telemetry Packet IDs
-#define ID_TELEMETRY_HIGH_NIBBLE        (uint8_t) DEVICE_GROUP_TELEMETRY << 4
-#define ID_TELEMETRY_SET_MAG_OFFSET     (uint8_t) ID_TELEMETRY_HIGH_NIBBLE | 0x00
+#define ID_TELEMETRY_SET_MAG_OFFSET     (uint8_t) 0x00
 
 // Priority bits
 #define PACKET_PRIORITY_HIGH            (uint8_t) 0x00
