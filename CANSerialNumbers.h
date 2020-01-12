6ea9588c6af9de6f2b43f91bb5db1df78fd7a091
DylanTomberlin <tomberln@uw.edu>
2020-01-11 18:08:17 -0800

    added Serial Numbers for rest of motors and power distro

--- CANSerialNumbers.h
+++ CANSerialNumbers.h
@@ -19,10 +19,27 @@
 #define DEVICE_SERIAL_JETSON            (uint8_t) 0x01
 
 // MOTOR UNIT GROUP
-#define DEVICE_SERIAL_BASE_ROT          (uint8_t) 0x01
-#define DEVICE_SERIAL_SHOULDER_ROT      (uint8_t) 0x02
-#define DEVICE_SERIAL_ELBOW_ROT         (uint8_t) 0x03
-#define DEVICE_SERIAL_WRIST_ROT         (uint8_t) 0x04
-#define DEVICE_SERIAL_DIFF_WRIST_ROT_1  (uint8_t) 0x05
-#define DEVICE_SERIAL_DIFF_WRIST_ROT_2  (uint8_t) 0x06
+#define DEVICE_SERIAL_MOTOR_BASE_ROT          (uint8_t) 0x01
+#define DEVICE_SERIAL_MOTOR_SHOULDER_ROT      (uint8_t) 0x02
+#define DEVICE_SERIAL_MOTOR_ELBOW_ROT         (uint8_t) 0x03
+#define DEVICE_SERIAL_MOTOR_WRIST_ROT         (uint8_t) 0x04
+#define DEVICE_SERIAL_MOTOR_DIFF_WRIST_ROT_1  (uint8_t) 0x05
+#define DEVICE_SERIAL_MOTOR_DIFF_WRIST_ROT_2  (uint8_t) 0x06
+#define DEVICE_SERIAL_MOTOR_DIFF_HAND         (uint8_t) 0x07
+
+#define DEVICE_SERIAL_MOTOR_CHASSIS_FL        (uint8_t) 0x08 //Front Left
+#define DEVICE_SERIAL_MOTOR_CHASSIS_FR        (uint8_t) 0x09
+#define DEVICE_SERIAL_MOTOR_CHASSIS_BL        (uint8_t) 0x0a 
+#define DEVICE_SERIAL_MOTOR_CHASSIS_BR        (uint8_t) 0x0c //Back Right
+
+        //if science has multiple motors, should name for function, not increment
+#define DEVICE_SERIAL_MOTOR_BSCIENCE_1         (uint8_t) 0x0d
+
+//Power Distro group
+#define DEVICE_SERIAL_POWER_CHASSIS_MAIN        (uint8_t) 0x01
+#define DEVICE_SERIAL_POWER_CHASSIS_DRIVE_1     (uint8_t) 0x02
+#define DEVICE_SERIAL_POWER_CHASSIS_DRIVE_2     (uint8_t) 0x03
+#define DEVICE_SERIAL_POWER_ARM_LOWER_1         (uint8_t) 0x04 //may have more arm
+#define DEVICE_SERIAL_POWER_ARM_UPPER_1         (uint8_t) 0x05
+#define DEVICE_SERIAL_POWER_SCIENCE             (uint8_t) 0x06
 
