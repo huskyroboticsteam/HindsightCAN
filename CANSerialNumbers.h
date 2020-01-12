81be71fb3351721837046dec7b593dcf53c4734d
DylanTomberlin <tomberln@uw.edu>
2020-01-12 01:21:24 -0800

    fixed some naming errors while documenting

--- CANSerialNumbers.h
+++ CANSerialNumbers.h
@@ -19,29 +19,30 @@
 #define DEVICE_SERIAL_JETSON            (uint8_t) 0x01
 
 // MOTOR UNIT GROUP
-#define DEVICE_SERIAL_MOTOR_BASE_ROT          (uint8_t) 0x01
-#define DEVICE_SERIAL_MOTOR_SHOULDER_ROT      (uint8_t) 0x02
-#define DEVICE_SERIAL_MOTOR_ELBOW_ROT         (uint8_t) 0x03
-#define DEVICE_SERIAL_MOTOR_WRIST_ROT         (uint8_t) 0x04
-#define DEVICE_SERIAL_MOTOR_DIFF_WRIST_ROT_1  (uint8_t) 0x05
-#define DEVICE_SERIAL_MOTOR_DIFF_WRIST_ROT_2  (uint8_t) 0x06
-#define DEVICE_SERIAL_MOTOR_DIFF_HAND         (uint8_t) 0x07
+#define DEVICE_SERIAL_MOTOR_BASE                (uint8_t) 0x01
+#define DEVICE_SERIAL_MOTOR_SHOULDER            (uint8_t) 0x02
+#define DEVICE_SERIAL_MOTOR_ELBOW               (uint8_t) 0x03
+#define DEVICE_SERIAL_MOTOR_FOREARM             (uint8_t) 0x04
+#define DEVICE_SERIAL_MOTOR_DIFF_WRIST_1        (uint8_t) 0x05
+#define DEVICE_SERIAL_MOTOR_DIFF_WRIST_2        (uint8_t) 0x06
+#define DEVICE_SERIAL_MOTOR_HAND                (uint8_t) 0x07
 
-#define DEVICE_SERIAL_MOTOR_CHASSIS_FL        (uint8_t) 0x08 //Front Left
-#define DEVICE_SERIAL_MOTOR_CHASSIS_FR        (uint8_t) 0x09
-#define DEVICE_SERIAL_MOTOR_CHASSIS_BL        (uint8_t) 0x0a 
-#define DEVICE_SERIAL_MOTOR_CHASSIS_BR        (uint8_t) 0x0c //Back Right
+#define DEVICE_SERIAL_MOTOR_CHASSIS_FL          (uint8_t) 0x08 //Front Left
+#define DEVICE_SERIAL_MOTOR_CHASSIS_FR          (uint8_t) 0x09
+#define DEVICE_SERIAL_MOTOR_CHASSIS_BL          (uint8_t) 0x0a 
+#define DEVICE_SERIAL_MOTOR_CHASSIS_BR          (uint8_t) 0x0b //Back Right
 
         //if science has multiple motors, should name for function, not increment
-#define DEVICE_SERIAL_MOTOR_BSCIENCE_1         (uint8_t) 0x0d
+#define DEVICE_SERIAL_MOTOR_SCIENCE_1           (uint8_t) 0x0c
 
-//Power Distro group
-#define DEVICE_SERIAL_POWER_CHASSIS_MAIN        (uint8_t) 0x01
-#define DEVICE_SERIAL_POWER_CHASSIS_DRIVE_1     (uint8_t) 0x02
-#define DEVICE_SERIAL_POWER_CHASSIS_DRIVE_2     (uint8_t) 0x03
-#define DEVICE_SERIAL_POWER_ARM_LOWER_1         (uint8_t) 0x04 //may have more arm
-#define DEVICE_SERIAL_POWER_ARM_UPPER_1         (uint8_t) 0x05
-#define DEVICE_SERIAL_POWER_SCIENCE             (uint8_t) 0x06
+//Power group
+#define DEVICE_SERIAL_POWER_BATT_MAN            (uint8_t) 0x01
+#define DEVICE_SERIAL_POWER_CHASSIS_MAIN        (uint8_t) 0x02
+#define DEVICE_SERIAL_POWER_CHASSIS_DRIVE_L     (uint8_t) 0x03 //Left
+#define DEVICE_SERIAL_POWER_CHASSIS_DRIVE_R     (uint8_t) 0x04 //Right
+#define DEVICE_SERIAL_POWER_ARM_LOWER_1         (uint8_t) 0x05 //may have more arm
+#define DEVICE_SERIAL_POWER_ARM_UPPER_1         (uint8_t) 0x06
+#define DEVICE_SERIAL_POWER_SCIENCE             (uint8_t) 0x07
 
 //Telemetry group
 #define DEVICE_SERIAL_TELEM_LOCALIZATION        (uint8_t) 0x01
