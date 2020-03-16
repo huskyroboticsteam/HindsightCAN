f48094c3fc3d1d67df7129d0184b228e2d836534
DylanTomberlin <tomberln@uw.edu>
2020-03-15 23:32:56 -0700

    Added Device Group Number consts

--- CANSerialNumbers.h
+++ CANSerialNumbers.h
@@ -51,3 +51,11 @@
 #define DEVICE_SERIAL_TELEM_TEMPERATURE         (uint8_t) 0x03
 #define DEVICE_SERIAL_TELEM_SCIENCE             (uint8_t) 0x04
 
+//Group numbers
+#define DEVICE_GROUP_BROADCAST  0x0
+#define DEVICE_GROUP_RESERVED   0x1
+#define DEVICE_GROUP_MASTER     0x2
+#define DEVICE_GROUP_POWER      0x3
+#define DEVICE_GROUP_MOTORS     0x4
+#define DEVICE_GROUP_TELEM      0x5
+#define DEVICE_GROUP_GPIO       0x6
