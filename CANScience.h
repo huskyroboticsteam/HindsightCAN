6b576015d804459c9bc2946a6a35d95c0b281ffc
Evan Straw <evan.straw99@gmail.com>
2022-05-29 13:41:45 -0700

    Add science board serial numbers, packet IDs, and struct marshaling

--- CANScience.h
+++ CANScience.h
@@ -5,34 +5,33 @@
 #define CAN_SCIENCE_SENSOR_UV PACKET_TELEMETRY_SENSOR2
 #define CAN_SCIENCE_SENSOR_MOISTURE PACKET_TELEMETRY_SENSOR3
 
-#define ID_SCIENCE_ENCODER_REPORT 0x0F
-#define ID_SCIENCE_SERVO_SET         (uint8_t) 0x0D
+#define ID_SCIENCE_LAZY_SUSAN_POS_SET   ((uint8_t) 0x0C)
+#define ID_SCIENCE_SERVO_SET            ((uint8_t) 0x0D)
+#define ID_SCIENCE_CONT_SERVO_POWER_SET ((uint8_t) 0x0E)
 
 #include "CANPacket.h"
 
-void AssembleScienceSensorPullPacket(CANPacket *packetToAssemble,
-uint8_t targetGroup,
-uint8_t targetSerial,
-uint8_t sensorCode);
-
-void AssembleScienceMotorControlPacket(CANPacket *packetToAssemble,
-uint8_t targetGroup,
-uint8_t targetSerial,
-uint8_t motor,
-int16_t PWMSet);
-
 void AssembleScienceServoPacket(CANPacket *packetToAssemble,
-uint8_t targetGroup,
-uint8_t targetSerial,
-uint8_t servo,
-uint8_t degrees);
+                                uint8_t targetGroup, uint8_t targetSerial,
+                                uint8_t servo, uint8_t degrees);
 
-int16_t GetScienceMotorPWMFromPacket(CANPacket *packet);
+void AssembleScienceLazySusanPosSetPacket(CANPacket *packetToAssemble,
+                                          uint8_t targetDeviceGroup,
+                                          uint8_t targetDeviceSerial,
+                                          uint8_t position);
 
-uint8_t GetScienceMotorIDFromPacket(CANPacket *packet);
+void AssembleScienceContServoPowerSetPacket(CANPacket *packetToAssemble,
+											uint8_t targetDeviceGroup,
+											uint8_t targetDeviceSerial,
+											uint8_t servo,
+											int8_t power);
 
 uint8_t GetScienceServoIDFromPacket(CANPacket *packet);
 
 uint8_t GetScienceServoAngleFromPacket(CANPacket *packet);
+
+uint8_t GetScienceLazySusanPosFromPacket(CANPacket *packet);
+
+int8_t GetScienceContServoPowerFromPacket(CANPacket *packet);
 
 #endif
