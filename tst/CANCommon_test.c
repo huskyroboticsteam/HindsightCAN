c4fe068eb91672d5446052044e195cc587996444
Michael <tsienmd@cs.washington.edu>
2025-04-26 12:23:43 -0700

    Create CANCommon_test.c
    initial CANCommon_test file

---
+++ tst/CANCommon_test.c
@@ -0,0 +1,22 @@
+#include "./unity/unity.h"
+
+#include "../CANCommon.h"
+
+void setUp() {}    // Runs before each test
+void tearDown() {} // Runs after each test
+
+void assembleEmergencyStopPacket_withAddressZero_createsCorrectPacket()
+{
+    CANPacket *testPacket;
+
+    AssembleEmergencyStopPacket(testPacket, 0, 0, 1);
+}
+
+int main()
+{
+    UNITY_BEGIN();
+
+    RUN_TEST(assembleEmergencyStopPacket_withAddressZero_createsCorrectPacket);
+
+    return UNITY_END();
+}
