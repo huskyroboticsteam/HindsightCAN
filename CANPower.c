fab4ae5160a45dd807cd23753fe299938192fc9e
Isaac <isaacw1925@gmail.com>
2025-05-28 19:45:34 -0700

    Raw CAN Packet assmebler

--- CANPower.c
+++ CANPower.c
@@ -9,6 +9,15 @@
 #include "CANPower.h"
 #include "CANPacket.h"
 
+void AssembleCANPacket(CANPacket *p, uint8_t priority, uint8_t group, uint8_t serial, uint8_t pid, uint8_t dlc, uint8_t *data) {
+    p->id = ConstructCANID(priority, group, serial);
+    p->dlc = dlc;
+	WritePacketIDOnly(p->data, pid);
+    for (int i = 1; i < dlc; i++) {
+        p->data[i] = data[i-1];
+    }
+}
+
 //Power Rail Set State
 void AssemblePowerRailsSetPacket(CANPacket *packetToAssemble,
     uint8_t targetDeviceGroup,
